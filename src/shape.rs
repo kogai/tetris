ba41270d41c7cdf7f18b50236ef07fc717db073f
小飼慎一 <kogai0121@gmail.com>
2017-05-13 14:47:23 +0900

    fix update fn

--- src/shape.rs
+++ src/shape.rs
@@ -6,54 +6,68 @@ pub type PosRow = u8;
 
 #[derive(Debug)]
 pub struct Inner {
-  grid: Grid,
-  pos_x: PosColumn,
-  pos_y: PosRow,
+    grid: Grid,
+    pos_x: PosColumn,
+    pos_y: PosRow,
 }
 
 impl Inner {
     fn with_fall(&self) -> Self {
-        Inner { grid: self.grid.clone(), pos_x: self.pos_x, pos_y: self.pos_y + 1 }
+        Inner {
+            grid: self.grid.clone(),
+            pos_x: self.pos_x,
+            pos_y: self.pos_y + 1,
+        }
     }
 
     fn get_positions(&self) -> Vec<(PosRow, PosColumn)> {
-        self.grid.iter().enumerate().flat_map(|(offset_y, row)| {
-            row.iter().enumerate().map(|(offset_x, _)| {
-                (offset_y as PosRow + self.pos_y, offset_x as PosColumn + self.pos_x)
-            }).collect::<Vec<_>>()
-        }).collect::<Vec<_>>()
+        self.grid
+            .iter()
+            .enumerate()
+            .flat_map(|(offset_y, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(offset_x, _)| {
+                        (offset_y as PosRow + self.pos_y, offset_x as PosColumn + self.pos_x)
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
     }
 }
 
 #[derive(Debug)]
 pub enum Shape {
-  Square(Inner),
-  BracketL(Inner),
-  BracketR(Inner),
-  Straight(Inner),
-  TLike(Inner),
+    Square(Inner),
+    BracketL(Inner),
+    BracketR(Inner),
+    Straight(Inner),
+    TLike(Inner),
 }
 
 impl Shape {
     pub fn square() -> Self {
         Shape::Square(Inner {
-          grid: vec![
-            vec![1,1],
-            vec![1,1],
-          ],
-          pos_x: 0,
-          pos_y: 0,
+            grid: vec![
+                vec![1, 1], vec![1, 1]
+            ],
+            pos_x: 0,
+            pos_y: 0,
         })
     }
-    
-    // pub fn bracket_l() -> Self {
-    //     Shape::BracketL(vec![
-    //       vec![1,0],
-    //       vec![1,0],
-    //       vec![1,1],
-    //     ], 0, 0)
-    // }
-    
+
+    pub fn bracket_l() -> Self {
+        Shape::BracketL(Inner {
+            grid: vec![
+                vec![1],
+                vec![1],
+                vec![1, 1]
+            ],
+            pos_x: 0,
+            pos_y: 0,
+        })
+    }
+
     // pub fn bracket_r() -> Self {
     //     Shape::BracketR(vec![
     //       vec![0,1],
@@ -63,14 +77,14 @@ impl Shape {
     // }
 
     pub fn tick(&mut self) -> Self {
-      use self::Shape::*;
-      match self {
-          &mut Square(ref inner) => Square(inner.with_fall()),
-          &mut BracketL(ref inner) => BracketL(inner.with_fall()),
-          &mut BracketR(ref inner) => BracketR(inner.with_fall()),
-          &mut Straight(ref inner) => Straight(inner.with_fall()),
-          &mut TLike(ref inner) => TLike(inner.with_fall()),
-      }
+        use self::Shape::*;
+        match self {
+            &mut Square(ref inner) => Square(inner.with_fall()),
+            &mut BracketL(ref inner) => BracketL(inner.with_fall()),
+            &mut BracketR(ref inner) => BracketR(inner.with_fall()),
+            &mut Straight(ref inner) => Straight(inner.with_fall()),
+            &mut TLike(ref inner) => TLike(inner.with_fall()),
+        }
     }
 
     pub fn get_positions(&self) -> Vec<(PosRow, PosColumn)> {
@@ -87,13 +101,13 @@ impl Shape {
 
 impl Block for Shape {
     fn show(&self) -> String {
-      use self::Shape::*;
-      match self {
-          &Square(ref inner) |
-          &BracketL(ref inner) |
-          &BracketR(ref inner) |
-          &Straight(ref inner) |
-          &TLike(ref inner) => show(&inner.grid),
-      }
+        use self::Shape::*;
+        match self {
+            &Square(ref inner) |
+            &BracketL(ref inner) |
+            &BracketR(ref inner) |
+            &Straight(ref inner) |
+            &TLike(ref inner) => show(&inner.grid),
+        }
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -1,43 +1,41 @@
-use shape::{self, PosRow, PosColumn, Grid};
+use shape::{PosRow, PosColumn, Grid, Shape};
 
 #[derive(Debug)]
 pub struct World {
-    grid: Grid
+    grid: Grid,
+    shape_queue: Vec<Shape>,
 }
 
 impl World {
     pub fn new(num_of_columns: u8, num_of_rows: u8) -> Self {
         World {
-          grid: (0..num_of_rows)
-            .map(
-              |_| (0..num_of_columns).map(|_| 0).collect()
-            ).collect()
+            grid: (0..num_of_rows)
+                .map(|_| (0..num_of_columns).map(|_| 0).collect())
+                .collect(),
+            shape_queue: vec![],
         }
     }
 
     fn fill_world(&self, to_fill: Vec<(PosRow, PosColumn)>) -> Grid {
-        let to_fill_rows = &to_fill.iter().map(|&(row, _)| row).collect::<Vec<_>>();
-        let to_fill_columns = &to_fill.iter().map(|&(_, column)| column).collect::<Vec<_>>();
-
-        self.grid.iter().enumerate().map(
-            |(i, row)| {
-                let should_update_row = to_fill_rows.iter().any(|r| *r as usize == i);
-                if should_update_row {
-                    row.iter().enumerate().map(|(j, col)| {
-                        let should_update_column = to_fill_columns.iter().any(|c| *c as usize == j);
-                        if should_update_column { 1 } else { 0 }
-                    }).collect::<Vec<_>>()
-                } else {
-                    row.clone()
-                }
-            }
-        ).collect::<Vec<_>>()
+        self.grid
+            .iter()
+            .enumerate()
+            .map(|(i, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(j, _)| {
+                        let is_matched_position = to_fill.iter().any(|&(r, c)| r as usize == i && c as usize == j);
+                        if is_matched_position { 1 } else { 0 }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
     }
 
     pub fn tick(&mut self) {
-        let square = shape::Shape::square();
-        let to_fill = square.get_positions();
-        self.grid = self.fill_world(to_fill); 
+        let bracket_l = Shape::bracket_l();
+        let to_fill = bracket_l.get_positions();
+        self.grid = self.fill_world(to_fill);
     }
 }
 
@@ -48,13 +46,16 @@ impl Block for World {
 }
 
 pub trait Block {
-  fn show(&self) -> String;
+    fn show(&self) -> String;
 }
 
 pub fn show(grid: &Grid) -> String {
     let white = " 0"; // "\u{26aa}"
     let black = " 1"; // "\u{26ab}"
-    grid.iter().map(
-      |row| row.iter().map(|x| if *x == 0 { white } else { black } ).collect::<Vec<_>>().join("")
-    ).collect::<Vec<_>>().join("\n")
+    grid.iter()
+        .map(|row| {
+            row.iter().map(|x| if *x == 0 { white } else { black }).collect::<Vec<_>>().join("")
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
 }
