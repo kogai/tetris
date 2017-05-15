bc9559b92ff95da9e8a4d603b6e481171f7d1d2c
小飼慎一 <kogai0121@gmail.com>
2017-05-15 22:13:38 +0900

    Loop with interval

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,16 @@ use command::{Command, Controller};
 
 static COLUMNS: u8 = 10;
 static ROWS: u8 = 14;
-static INTERVAL: u64 = 10;
-static MAX_LOOP: u8 = 4;
+static INTERVAL: u64 = 1000;
 
 fn main() {
     let (tx, rx) = channel::<_>();
     let controller = Controller::new(tx);
     let mut game = World::new(COLUMNS, ROWS, rx);
-    
+
     controller.fall_with_interval();
 
-    let mut tick_count = 0;
     loop {
-        if tick_count > MAX_LOOP {
-            break;
-        }
-        tick_count = tick_count + 1;
-
-        match tick_count {
-            1 => controller.send(Command::Left),
-            2 => controller.send(Command::Right),
-            _ => controller.send(Command::Bottom),
-        };
-        
         game.tick();
 
         println!("{}", game.show());
